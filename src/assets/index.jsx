48ba56170042fa7e83fb8fb78d37a65293e5617d
AllenWangyc <451366033@qq.com>
2025-05-11 02:49:22 +1200

    add skill page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -6,4 +6,27 @@ import githubIcon from './github.png';
 import linkedinIcon from './linkedin.png';
 import igIcon from './ig.png';
 
-export { fbIcon, githubIcon, linkedinIcon, igIcon, avatar }
+import csIcon from './c-sharp.png';
+import cssIcon from './css-3.png';
+import figmaIcon from './figma.png';
+import htmlIcon from './html-5.png';
+import jsIcon from './js.png';
+import nodejsIcon from './node-js.png';
+import reactIcon from './physics.png';
+import postgreIcon from './postgre.png';
+import typescriptIcon from './typescript.png'
+import stylusIcon from './stylus.png';
+import mongodbIcon from './mongodb.png';
+import dockerIcon from './docker.png';
+import awsIcon from './aws.png';
+import pythonIcon from './python.png';
+import githubIcon2 from './github-skill.png';
+
+
+export { fbIcon, githubIcon, linkedinIcon, igIcon, avatar };
+
+export {
+  csIcon, cssIcon, figmaIcon, htmlIcon, jsIcon, nodejsIcon, awsIcon,
+  reactIcon, postgreIcon, typescriptIcon, stylusIcon, mongodbIcon, dockerIcon,
+  pythonIcon, githubIcon2
+};
--- src/pages/Skill/index.jsx
+++ src/pages/Skill/index.jsx
@@ -1,10 +1,40 @@
-import { SectionHeader } from '../../components'
-import './skill.styl'
+import React from 'react';
+import { SectionHeader } from '../../components';
+import './skill.styl';
+import {
+  csIcon, cssIcon, htmlIcon, jsIcon, nodejsIcon, awsIcon,
+  reactIcon, postgreIcon, typescriptIcon, dockerIcon, mongodbIcon, pythonIcon,
+  githubIcon2
+} from '../../assets';
+
+const skills = [
+  { name: 'HTML5', Icon: htmlIcon },
+  { name: 'CSS3', Icon: cssIcon },
+  { name: 'JavaScript', Icon: jsIcon },
+  { name: 'TypeScript', Icon: typescriptIcon },
+  { name: 'React', Icon: reactIcon },
+  { name: 'Node.js', Icon: nodejsIcon },
+  { name: 'C#', Icon: csIcon },
+  { name: 'Python', Icon: pythonIcon },
+  { name: 'PostgreSQL', Icon: postgreIcon },
+  { name: 'MongoDB', Icon: mongodbIcon },
+  { name: 'AWS', Icon: awsIcon },
+  { name: 'Docker', Icon: dockerIcon },
+  { name: 'GitHub', Icon: githubIcon2 },
+]
 
 const Skill = () => {
   return (
     <div className='P-skill'>
       <SectionHeader title='Skill' />
+      <div className="skills-grid">
+        {skills.map(({ name, Icon }, idx) => (
+          <div className="skill-item" key={idx}>
+            <img src={Icon} alt={name} className="skill-icon" />
+            <span className="skill-tooltip">{name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
