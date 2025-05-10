6df3dd58d2820b6c9b02b91195cf8c98cadb1745
AllenWangyc <451366033@qq.com>
2025-05-10 16:10:35 +1200

    add fade-in-up effect

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import Typewriter from '../../components/TypeWriter';
 import './home.styl';
 
 const positions = ['Web Developer ', "Cloud Practitioner ", "Full-stack Developer ", "Software Engineer "];
 
 const Home = () => {
+  const [mounted, setMounted] = useState(true);
+  useEffect(() => {
+    setMounted(true);
+  })
   return (
     <div className='hero'>
       <div className="hero-text">
-        <h1>Yuchen Wang,</h1>
-        <Typewriter
+        <h1 className={`fade ${mounted ? 'in delay-0' : ''}`}>
+          Yuchen Wang,
+        </h1>
+        <p className={`fade ${mounted ? 'in delay-1' : ''}`}>
+          <Typewriter words={positions} speed={120} pause={2500} />
+        </p>
+        {/* <Typewriter
           words={positions}
           speed={120}
           pause={2500}
-        />
+        /> */}
       </div>
     </div>
   )
