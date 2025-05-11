9f09ee42e059774e0bc98e93048feabb8f1867ec
AllenWangyc <451366033@qq.com>
2025-05-12 09:10:59 +1200

    add resume download button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
-import { Layout, Menu, ConfigProvider, Avatar } from 'antd';
-import { ProjectOutlined, UserOutlined, HomeOutlined, ReadOutlined, FileTextOutlined, MailOutlined } from '@ant-design/icons';
+import { Layout, Menu, ConfigProvider, Avatar, Button } from 'antd';
+import { ProjectOutlined, UserOutlined, HomeOutlined, ReadOutlined, DownloadOutlined, MailOutlined } from '@ant-design/icons';
 import { Home, About, Skill, Projects, Contact } from './pages';
 import './App.styl';
 import { fbIcon, githubIcon, linkedinIcon, igIcon, avatar } from './assets';
@@ -79,19 +79,46 @@ function App() {
               <Menu mode="inline" defaultSelectedKeys={['home']} items={items} onClick={handleClick} />
             </ConfigProvider>
 
-            <div className="social-container">
-              <a href="https://linkedin.com/in/yuchen-wang-42b920272" target="_blank" rel="noreferrer">
-                <img src={linkedinIcon} alt="LinkedIn" />
-              </a>
-              <a href="https://github.com/AllenWangyc" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="GitHub" />
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100090374055425" target="_blank" rel="noreferrer">
-                <img src={fbIcon} alt="Facebook" />
-              </a>
-              <a href="https://www.instagram.com/omoshiroaw" target="_blank" rel="noreferrer">
-                <img src={igIcon} alt="Instagram" />
-              </a>
+            <div className='bottom-wrapper'>
+              <div className="resume-btn-wrapper">
+                <ConfigProvider
+                  theme={{
+                    components: {
+                      Button: {
+                        defaultBg: 'rgba(152,137,125,0.3)',
+                        defaultHoverBg: 'rgba(152,137,125,0.8)',
+                        defaultBorderColor: 'rgba(152,137,125,0.8)',
+                        defaultHoverBorderColor: 'rgba(152,137,125,1)',
+                        defaultHoverColor: 'rgb(226,226,226)'
+                      },
+                    },
+                  }}
+                >
+                  <Button
+                    variant='solid'
+                    icon={<DownloadOutlined />}
+                    size="large"
+                    href="/assets/resume.pdf"
+                    download
+                  >
+                    Resume
+                  </Button>
+                </ConfigProvider>
+              </div>
+              <div className="social-container">
+                <a href="https://linkedin.com/in/yuchen-wang-42b920272" target="_blank" rel="noreferrer">
+                  <img src={linkedinIcon} alt="LinkedIn" />
+                </a>
+                <a href="https://github.com/AllenWangyc" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100090374055425" target="_blank" rel="noreferrer">
+                  <img src={fbIcon} alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com/omoshiroaw" target="_blank" rel="noreferrer">
+                  <img src={igIcon} alt="Instagram" />
+                </a>
+              </div>
             </div>
           </Sider>
         </ConfigProvider>
