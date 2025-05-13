e9d2745b6be22f45572bd135ccddb95164ed8d2b
AllenWangyc <451366033@qq.com>
2025-05-13 15:15:24 +1200

    update resume

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         >
           <Sider className='sider'
             width="20%"
-            breakpoint="lg"
+            breakpoint="xl"
             collapsedWidth="0"
             onBreakpoint={broken => {
               console.log(broken);
@@ -63,61 +63,66 @@ function App() {
               </div>
             </div>
 
-            <ConfigProvider
-              theme={{
-                components: {
-                  Menu: {
-                    itemBg: 'rgb(226, 226, 226)',
-                    itemHeight: '3.5rem',
-                    fontSize: '1.3rem',
-                    iconSize: '1.3rem',
-                    iconMarginInlineEnd: '1.5rem'
-                  },
-                },
-              }}
-            >
-              <Menu mode="inline" defaultSelectedKeys={['home']} items={items} onClick={handleClick} />
-            </ConfigProvider>
-
-            <div className='bottom-wrapper'>
-              <div className="resume-btn-wrapper">
-                <ConfigProvider
-                  theme={{
-                    components: {
-                      Button: {
-                        defaultBg: 'rgba(152,137,125,0.3)',
-                        defaultHoverBg: 'rgba(152,137,125,0.8)',
-                        defaultBorderColor: 'rgba(152,137,125,0.8)',
-                        defaultHoverBorderColor: 'rgba(152,137,125,1)',
-                        defaultHoverColor: 'rgb(226,226,226)'
-                      },
+            <div className='menu-wrapper'>
+              <ConfigProvider
+                theme={{
+                  components: {
+                    Menu: {
+                      itemBg: 'rgb(226, 226, 226)',
+                      itemHeight: '3.5rem',
+                      fontSize: '1.3rem',
+                      iconSize: '1.3rem',
+                      iconMarginInlineEnd: '1.5rem'
                     },
-                  }}
-                >
-                  <Button
-                    variant='solid'
-                    icon={<DownloadOutlined />}
-                    size="large"
-                    href="/assets/resume.pdf"
-                    download
+                  },
+                }}
+              >
+                <div className="demo-logo-vertical" />
+                <Menu mode="inline" defaultSelectedKeys={['home']} items={items} onClick={handleClick} />
+              </ConfigProvider>
+            </div>
+
+            <div className='bottom-container'>
+              <div className='bottom-wrapper'>
+                <div className="resume-btn-wrapper">
+                  <ConfigProvider
+                    theme={{
+                      components: {
+                        Button: {
+                          defaultBg: 'rgba(152,137,125,0.3)',
+                          defaultHoverBg: 'rgba(152,137,125,0.8)',
+                          defaultBorderColor: 'rgba(152,137,125,0.8)',
+                          defaultHoverBorderColor: 'rgba(152,137,125,1)',
+                          defaultHoverColor: 'rgb(226,226,226)'
+                        },
+                      },
+                    }}
                   >
-                    Resume
-                  </Button>
-                </ConfigProvider>
-              </div>
-              <div className="social-container">
-                <a href="https://linkedin.com/in/yuchen-wang-42b920272" target="_blank" rel="noreferrer">
-                  <img src={linkedinIcon} alt="LinkedIn" />
-                </a>
-                <a href="https://github.com/AllenWangyc" target="_blank" rel="noreferrer">
-                  <img src={githubIcon} alt="GitHub" />
-                </a>
-                <a href="https://www.facebook.com/profile.php?id=100090374055425" target="_blank" rel="noreferrer">
-                  <img src={fbIcon} alt="Facebook" />
-                </a>
-                <a href="https://www.instagram.com/omoshiroaw" target="_blank" rel="noreferrer">
-                  <img src={igIcon} alt="Instagram" />
-                </a>
+                    <Button
+                      variant='solid'
+                      icon={<DownloadOutlined />}
+                      size="large"
+                      href="/assets/resume.pdf"
+                      download
+                    >
+                      Resume
+                    </Button>
+                  </ConfigProvider>
+                </div>
+                <div className="social-container">
+                  <a href="https://linkedin.com/in/yuchen-wang-42b920272" target="_blank" rel="noreferrer">
+                    <img src={linkedinIcon} alt="LinkedIn" />
+                  </a>
+                  <a href="https://github.com/AllenWangyc" target="_blank" rel="noreferrer">
+                    <img src={githubIcon} alt="GitHub" />
+                  </a>
+                  <a href="https://www.facebook.com/profile.php?id=100090374055425" target="_blank" rel="noreferrer">
+                    <img src={fbIcon} alt="Facebook" />
+                  </a>
+                  <a href="https://www.instagram.com/omoshiroaw" target="_blank" rel="noreferrer">
+                    <img src={igIcon} alt="Instagram" />
+                  </a>
+                </div>
               </div>
             </div>
           </Sider>
